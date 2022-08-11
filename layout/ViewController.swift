9a325b75f63795da359a7d1a63b24acdf137639f
Gizem <gizeemdemirr1@gmail.com>
2022-08-11 11:12:48 -0700

    layout first commit

---
+++ layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layout
+//
+//  Created by Gizem on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
